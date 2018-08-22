0639fc815e69cfe7d9c890b3cb435cf5ce02da5a
sean-ww <swallis@enrise.com>
2018-08-22 22:42:59 +0200

    GNIJ-155 testing

--- dev/release/changelogs/update/index.js
+++ dev/release/changelogs/update/index.js
@@ -4,18 +4,18 @@ const path = require('path');
 
 const changelogPath = path.join(__dirname, '../../../../docs/03-Changelog.md');
 const changeDirectory = path.join(__dirname, '../../../../changelogs/unreleased');
-const releaseVersion = getReleaseVersion(process.argv[2]);
-
+// const releaseVersion = getReleaseVersion(process.argv[2]);
+console.log(process.argv);
 // TODO: setup the hook after merging to a release branch
 // TODO: add tests to pipeline
 // TODO: update readme and changelog
 
-updateChangelog(changelogPath, changeDirectory, releaseVersion, true, error => {
-    if (error) {
-        throw error;
-    } else {
-        console.log('=================================================');
-        console.log(`Release ${releaseVersion} Changelog Updated`);
-        console.log('=================================================');
-    }
-});
+// updateChangelog(changelogPath, changeDirectory, releaseVersion, true, error => {
+//     if (error) {
+//         throw error;
+//     } else {
+//         console.log('=================================================');
+//         console.log(`Release ${releaseVersion} Changelog Updated`);
+//         console.log('=================================================');
+//     }
+// });
