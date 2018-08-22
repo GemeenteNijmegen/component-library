6bb48dc4cf856d40adc7b37322b8708d739a28c7
sean-ww <swallis@enrise.com>
2018-08-22 22:30:35 +0200

    GNIJ-155 testing

--- dev/release/changelogs/update/index.js
+++ dev/release/changelogs/update/index.js
@@ -5,19 +5,17 @@ const path = require('path');
 const changelogPath = path.join(__dirname, '../../../../docs/03-Changelog.md');
 const changeDirectory = path.join(__dirname, '../../../../changelogs/unreleased');
 const releaseVersion = getReleaseVersion(process.argv[2]);
-// Example: docker-compose run frontend node dev/release/changelogs/update '1.2.1'
+
 // TODO: setup the hook after merging to a release branch
-// TODO: create a pipeline and add tests
+// TODO: add tests to pipeline
 // TODO: update readme and changelog
 
-// updateChangelog(changelogPath, changeDirectory, releaseVersion, true, error => {
-//     if (error) {
-//         throw error;
-//     } else {
-//         console.log('Changelog updated');
-//     }
-// });
-
-console.log('===============================');
-console.log(`TESTING...${releaseVersion}`);
-console.log('===============================');
+updateChangelog(changelogPath, changeDirectory, releaseVersion, true, error => {
+    if (error) {
+        throw error;
+    } else {
+        console.log('=================================================');
+        console.log(`Release ${releaseVersion} Changelog Updated`);
+        console.log('=================================================');
+    }
+});
