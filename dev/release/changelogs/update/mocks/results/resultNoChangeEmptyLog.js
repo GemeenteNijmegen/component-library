6e06be6f4e5aff04196432a994c6909ab9834ca2
sean-ww <swallis@enrise.com>
2018-08-22 12:53:08 +0200

    wip: GNIJ-155 update changelog without conflicts

---
+++ dev/release/changelogs/update/mocks/results/resultNoChangeEmptyLog.js
@@ -0,0 +1,11 @@
+const resultNoChangeEmptyLog = `---
+title: Changelog
+status: ready # draft, ready
+---
+
+
+All notable changes to this project will be documented in this file.<br>
+The format is based on [Keep a Changelog](http://keepachangelog.com/).
+`;
+
+module.exports = { resultNoChangeEmptyLog };
