561382ffacdb888d79cdb0e1927efae54a7a50ce
sean-ww <swallis@enrise.com>
2018-08-23 10:21:28 +0200

    tester

--- dev/release/changelogs/update/index.js
+++ dev/release/changelogs/update/index.js
@@ -9,6 +9,7 @@ const releaseVersion = getReleaseVersion(process.argv[2]);
 // TODO: setup the hook after merging to a release branch
 // TODO: clear up unreleased folder after to prevent loop!
 // TODO: look at caching
+// TODO: add the version dynamically with testers/2 (just support major versions for now?)
 // TODO: tidy up ci putting back commented out and switching to release
 // TODO: merge back into 155
 // TODO: add tests to pipeline
