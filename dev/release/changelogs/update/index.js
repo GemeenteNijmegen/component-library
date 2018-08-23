d468651680fe1a66865861c7b40a41eb8c8e1e20
sean-ww <swallis@enrise.com>
2018-08-23 15:07:23 +0200

    tester

--- dev/release/changelogs/update/index.js
+++ dev/release/changelogs/update/index.js
@@ -6,8 +6,6 @@ const changelogPath = path.join(__dirname, '../../../../docs/03-Changelog.md');
 const changeDirectory = path.join(__dirname, '../../../../changelogs/unreleased');
 const releaseVersion = getReleaseVersion(process.argv[2]);
 
-// TODO: add tests to pipeline
-// TODO: tidy up ci putting back commented out and switching to release
 // TODO: merge back into 155
 // TODO: update readme and changelog
 
