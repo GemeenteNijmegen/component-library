973bd4c5cd3093b107c2f00cd3137147001b5d3b
sean-ww <swallis@enrise.com>
2018-08-23 12:48:41 +0200

    tester

--- dev/release/changelogs/update/index.js
+++ dev/release/changelogs/update/index.js
@@ -6,11 +6,10 @@ const changelogPath = path.join(__dirname, '../../../../docs/03-Changelog.md');
 const changeDirectory = path.join(__dirname, '../../../../changelogs/unreleased');
 const releaseVersion = getReleaseVersion(process.argv[2]);
 
-// TODO: look at caching
 // TODO: add the version dynamically with testers/2 (just support major versions for now?)
+// TODO: add tests to pipeline
 // TODO: tidy up ci putting back commented out and switching to release
 // TODO: merge back into 155
-// TODO: add tests to pipeline
 // TODO: update readme and changelog
 
 updateChangelog(changelogPath, changeDirectory, releaseVersion, true, error => {
