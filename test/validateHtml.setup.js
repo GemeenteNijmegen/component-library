2bdb449b74e39b87f312957b0b8c6ec5ceb3b748
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2019-08-30 14:10:59 +0200

    GNIJ-253 Fixed html validation errors

--- test/validateHtml.setup.js
+++ test/validateHtml.setup.js
@@ -6,6 +6,10 @@ const mockHelpers = [
     { name: 'mdbootstrapPath', return: 'http://mock/' },
     { name: 'assetPath', return: 'http://mock/' },
 ];
+const mockContext = { searchAction: '/' };
+const mock = () => {
+    mockPartials.map(partialName => handlebars.registerPartial(partialName, ''));
+    mockHelpers.map(helper => handlebars.registerHelper(helper.name, () => helper.return));
+};
 
-mockPartials.map(partialName => handlebars.registerPartial(partialName, ''));
-mockHelpers.map(helper => handlebars.registerHelper(helper.name, () => helper.return));
+module.exports = { mockContext, mock };
--- test/validateHtml.test.js
+++ test/validateHtml.test.js
@@ -4,14 +4,15 @@ const glob = require('glob');
 const { readFileSync } = require('fs');
 const handlebars = require('handlebars');
 
-require('./validateHtml.setup');
+const { mock, mockContext } = require('./validateHtml.setup');
 
 describe('Validate html for every component', () => {
+    mock();
     const files = glob.sync('components/!(_includes|templates)/**/!(_preview*).hbs');
     it.each(files)('The file %s should not have validation errors', async file => {
         const hbs = readFileSync(file).toString();
         const template = handlebars.compile(hbs);
-        const html = template();
+        const html = template(mockContext);
         const result = JSON.parse(
             await validator({
                 isFragment: true,
