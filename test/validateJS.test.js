a3141536e3c829595535361351b95a7fca21dbb3
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2019-09-17 15:49:59 +0200

    GNIJ-263 Add es5 validator to the tests

---
+++ test/validateJS.test.js
@@ -0,0 +1,30 @@
+/* eslint-env jest */
+const { JSHINT } = require('jshint');
+const jquery = require('jquery');
+const glob = require('glob');
+const { readFileSync } = require('fs');
+
+describe('Validate js for all scripts', () => {
+    const jshintConfig = JSON.parse(readFileSync('components/_includes/scripts/.jshintrc'));
+    const files = glob.sync('components/_includes/scripts/**/*.hbs');
+    it.each(files)('The script %s should be valid es5', async file => {
+        const contents = readFileSync(file).toString();
+        const html = jquery.parseHTML(contents, null, true);
+        const errors = html.reduce((errors, element) => {
+            if (element.type == '' || element.type == 'text/javascript') {
+                JSHINT(element.text, jshintConfig, {});
+                return errors.concat(
+                    JSHINT.errors.map(error => ({
+                        line: error.line,
+                        reason: error.reason,
+                        code: error.evidence,
+                        errorType: error.code,
+                    })),
+                );
+            }
+            return errors;
+        }, []);
+
+        expect(errors).toHaveLength(0);
+    });
+});
