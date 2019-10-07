3fe6e976fa9788f22b99ec35901906d46c4f559b
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2019-10-07 16:28:05 +0200

    GNIJ-273 Add gitlab stages

--- test/regression/features/step_definitions/component.js
+++ test/regression/features/step_definitions/component.js
@@ -2,6 +2,7 @@ const { When } = require('cucumber');
 const openUrl = require('cucumber-puppeteer/features/support/action/openUrl');
 
 When('I open the component {string}', async function(componentName) {
-    const url = `/components/preview/${componentName}`;
+    const ext = process.env.COMPONENT_EXT ? process.env.COMPONENT_EXT : '';
+    const url = `/components/preview/${componentName}${ext}`;
     await openUrl.call(this, url);
 });
