909d1cfd1fcc9a53269e8036fcfbb063e2017c4c
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2019-10-15 08:42:47 +0200

    GNIJ-272 Disable all transitions for testing

--- test/regression/features/step_definitions/component.js
+++ test/regression/features/step_definitions/component.js
@@ -1,10 +1,10 @@
 const { When } = require('cucumber');
 const openUrl = require('cucumber-puppeteer/features/support/action/openUrl');
 
-const noCaretStyle = { content: '* { caret-color: transparent !important; }' };
+const globalStyle = { content: '* { caret-color: transparent !important; transition: none !important }' };
 
 When('I open the component {string}', async function(componentName) {
     const url = `/components/preview/${componentName}`;
     await openUrl.call(this, url);
-    await this.page.addStyleTag(noCaretStyle);
+    await this.page.addStyleTag(globalStyle);
 });
