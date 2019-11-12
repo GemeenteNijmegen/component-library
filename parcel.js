32077aa3655e2d2bcfe65e583c057c1f504cfbf8
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2019-11-12 13:58:53 +0100

    GNIJ-289 Add build command

--- parcel.js
+++ parcel.js
@@ -10,15 +10,33 @@ const options = {
     contentHash: false,
 };
 
-(async function() {
+const runDev = async function() {
     const logger = fractal.cli.console;
     const server = fractal.web.server({
         sync: true,
     });
 
     const bundler = new Bundler(entryFiles, options);
-    const bundle = await bundler.bundle();
-
+    await bundler.bundle();
     await server.start();
     logger.success(`Fractal server is now running at ${server.url}`);
-})();
+};
+
+const build = async function() {
+    const logger = fractal.cli.console;
+    const builder = fractal.web.builder();
+
+    const bundler = new Bundler(entryFiles, options);
+    logger.success('Start bundling assets');
+    await bundler.bundle();
+    logger.success('Assets bundled');
+    logger.success('Start building fractal');
+    await builder.build();
+    logger.success('Fractal build completed!');
+};
+
+if (process.env.NODE_ENV === 'development') {
+    runDev();
+} else {
+    build();
+}
