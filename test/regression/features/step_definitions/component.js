5ccc4fedab128e331d7fb409ed9c06e683892325
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2020-02-28 15:38:42 +0100

    GNIJ-318 Fix random pipeline failures

--- test/regression/features/step_definitions/component.js
+++ test/regression/features/step_definitions/component.js
@@ -2,11 +2,25 @@ const { When } = require('cucumber');
 const openUrl = require('cucumber-puppeteer/features/support/action/openUrl');
 const waitFor = require('cucumber-puppeteer/features/support/action/waitFor');
 
-const globalStyle = { content: '* { caret-color: transparent !important; transition: none !important }' };
+const globalStyle = {
+    content: `
+*,
+*::after,
+*::before {
+    transition-delay: 0s !important;
+    transition-duration: 0s !important;
+    animation-delay: -0.0001s !important;
+    animation-duration: 0s !important;
+    animation-play-state: paused !important;
+    caret-color: transparent !important;
+    color-adjust: exact !important;
+    transition: none !important;
+}`,
+};
 
 When('I open the component {string}', async function(componentName) {
     const url = `/components/preview/${componentName}`;
-    await Promise.all([this.page.waitForNavigation({ waitUntil: 'domcontentloaded' }), openUrl.call(this, url)]);
+    await Promise.all([this.page.waitForNavigation({ waitUntil: 'networkidle0' }), openUrl.call(this, url)]);
     await this.page.addStyleTag(globalStyle);
     await waitFor.call(this, 0.5);
 });
