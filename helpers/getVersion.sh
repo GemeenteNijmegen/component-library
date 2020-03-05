494b70eaf70c1cb8df877b06e0fba1114943320e
Tim de Pater <git@trafex.nl>
2020-03-05 14:31:57 +0100

    GNIJ-308 Setup pipeline for deployment to S3

---
+++ helpers/getVersion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CURRENT_FOLDER=$(dirname "$0")
+PACKAGE_VERSION=$(
+    grep version < "$CURRENT_FOLDER/../package.json" \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",\t ]//g')
+
+echo "$PACKAGE_VERSION"
