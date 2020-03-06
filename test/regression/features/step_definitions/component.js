c8e9b254df18fda8601451b44c7bcdef77b59977
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2020-03-06 10:03:20 +0100

    GNIJ-308 Add status message when loading of a component failed

--- test/regression/features/step_definitions/component.js
+++ test/regression/features/step_definitions/component.js
@@ -1,12 +1,17 @@
 const { When } = require('cucumber');
 const openUrl = require('cucumber-puppeteer/features/support/action/openUrl');
 const waitFor = require('cucumber-puppeteer/features/support/action/waitFor');
+const { expect } = require('chai');
 
 const globalStyle = { content: '* { caret-color: transparent !important; transition: none !important }' };
 
 When('I open the component {string}', async function(componentName) {
     const url = `/components/preview/${componentName}`;
-    await Promise.all([this.page.waitForNavigation({ waitUntil: 'domcontentloaded' }), openUrl.call(this, url)]);
+    const [, response] = await Promise.all([
+        this.page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
+        openUrl.call(this, url),
+    ]);
     await this.page.addStyleTag(globalStyle);
     await waitFor.call(this, 0.5);
+    expect(response.status(), `Loading url: ${url} failed`).to.be.within(200, 299);
 });
