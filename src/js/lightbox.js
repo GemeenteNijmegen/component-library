7b524d5ac6f11f16abcaefed837886bc9df0af2c
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2020-05-06 14:10:47 +0200

    GNIJ-332 Fix other component that broke

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,3 +1,4 @@
-const lightboxUi = require('../../static/mdb-addons/mdb-lightbox-ui.html')
-
-$('#mdb-lightbox-ui').html(lightboxUi);
+(async () => {
+    const lightboxUi = await import('../../static/mdb-addons/mdb-lightbox-ui.html');
+    $('#mdb-lightbox-ui').html(lightboxUi);
+})();
