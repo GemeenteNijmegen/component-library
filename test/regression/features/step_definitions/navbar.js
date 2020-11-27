4aae388245d9ec61359c8a72215bcbb8bdd2075e
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2020-11-27 17:05:55 +0100

    GNIJ-347 Fix selector

--- test/regression/features/step_definitions/navbar.js
+++ test/regression/features/step_definitions/navbar.js
@@ -8,7 +8,7 @@ When('I press enter on an internal link', async function() {
 });
 
 When('I press enter on an internal link for a different page', async function() {
-    await pressKey.call(this, 'Enter', '.navbar-nav .nav-link:nth-child(2)');
+    await pressKey.call(this, 'Enter', '.navbar-nav .nav-item:nth-child(2) a');
 });
 
 When('I press enter on an external link', async function() {
