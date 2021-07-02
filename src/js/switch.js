79b604dad56d0ebf38db4ca8dcc23ec941d69f04
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2021-07-02 12:07:19 +0200

    GNIJ-372 Remove the aria-checked from the switch component

--- src/js/switch.js
+++ src/js/switch.js
@@ -7,9 +7,3 @@ for (i = 0; i < elList.length; i++) {
     var a11ySwitch = new A11YswitchCheck();
     a11ySwitch.init(elList[i]);
 }
-
-$('.check-switch input')
-    .off('click')
-    .on('click', function() {
-        $(this).attr('aria-checked', $(this).is(':checked'));
-    });
