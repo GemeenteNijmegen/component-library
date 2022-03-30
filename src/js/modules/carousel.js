aad4d16a1c19baf221d9f997afbc3ece71707e94
Rowin Mol <rmol@enrise.com>
2022-03-30 07:59:34 +0200

    GNIJ-379 - improve accesibility carousel functionality

--- src/js/modules/carousel.js
+++ src/js/modules/carousel.js
@@ -6,23 +6,23 @@ Carousel.prototype.init = function(element, options) {
     this.$carousel = $(element);
     const currentOptions = options ? options : { interval: 2000, pause: false };
     this.$carousel.carousel(currentOptions);
-    const carouselRef = this;
+
+    $('li[data-target="#carousel-example"]').each(function() {
+        $(this).attr('tabindex', '0');
+    });
 
     $('.carousel-indicators li').on('keypress', function(event) {
-        const enterEvent = 13;
-
-        if (event.which == enterEvent) {
-            const targetIndex = event.target.getAttribute('data-slide-to');
-            carouselRef.$carousel.carousel(Number(targetIndex));
-            carouselRef.pause();
+        if (event.key == 'Enter') {
+            $(this).trigger('click');
+            $('.carousel-play-pause .carousel-control-pause').trigger('click');
         }
     });
 
-    $('.carousel-control-play', this.$carousel).click(this.play.bind(this));
-    $('.carousel-control-pause', this.$carousel).click(this.pause.bind(this));
+    $('.carousel-control-play', this.$carousel).on('click', this.play.bind(this));
+    $('.carousel-control-pause', this.$carousel).on('click', this.pause.bind(this));
 
-    $('.carousel-control-prev', this.$carousel).click(this.pause.bind(this));
-    $('.carousel-control-next', this.$carousel).click(this.pause.bind(this));
+    $('.carousel-control-prev', this.$carousel).on('click', this.pause.bind(this));
+    $('.carousel-control-next', this.$carousel).on('click', this.pause.bind(this));
 };
 
 Carousel.prototype.play = function() {
