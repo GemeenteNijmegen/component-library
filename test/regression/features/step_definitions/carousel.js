df3f792d5fd7b93dc9bca1ec8add0b64e8c9e175
Henk-Jan van Voorthuijsen <hjvvoorthuijsen@enrise.com>
2022-04-06 13:46:13 +0200

    Use newer version of puppeteer-cucumber It includes the latest axe-core and chromium 99

--- test/regression/features/step_definitions/carousel.js
+++ test/regression/features/step_definitions/carousel.js
@@ -1,6 +1,5 @@
 const { When } = require('cucumber');
 const clickElement = require('cucumber-puppeteer/features/support/action/clickElement');
-const waitFor = require('cucumber-puppeteer/features/support/action/waitFor');
 
 When('I pause the carousel on slide nr {int}', async function(slideId) {
     await clickElement.call(this, '.carousel-control-pause');
@@ -9,5 +8,5 @@ When('I pause the carousel on slide nr {int}', async function(slideId) {
         `.carousel-indicators > li:nth-child(${slideId})`,
         `.carousel-inner > .carousel-item:nth-child(${slideId + 1})`,
     );
-    await waitFor.call(this, 0.6);
+    await this.page.waitForTimeout(600);
 });
